b49ed74e70dab8c4a3f2aa4b6b1d3333a5383d98
Jorge Cuellar <150642412+Masio84@users.noreply.github.com>
2024-01-26 02:16:46 -0500

    Juego del Número Secreto en Alura LATAM
    Versión Final del Juego del Número Secreto

--- app.js
+++ app.js
@@ -1,53 +1,76 @@
-/*alert('Bienvenido al juego del número secreto');
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
 
-// Agrega un console.log para verificar el valor de "intento" después de la entrada del usuario
-let intento = prompt('Elige un número entre 1 y 10');
-console.log('Valor de intento:', intento);
 
-let numeroSecreto = 4;
 
-// Agrega un console.log para verificar la comparación entre "intento" y "numeroSecreto"
-console.log('Resultado de la comparación:', intento == numeroSecreto);
-
-if (intento == numeroSecreto) {
-    alert('Adivinaste');
-} else {
-    // Agrega un console.log para verificar el valor de "numeroSecreto" cuando el jugador se equivoca
-    console.log('Valor del número secreto:', numeroSecreto);
-    alert('El número secreto era ' + numeroSecreto);
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
-*/
-//Variables
-let numeroSecreto = Math.floor(Math.random()*100)+1;
-let numeroUsuario = 0;
-let intentos = 1;
-//let palabraTurno = "turno";
-console.log(numeroSecreto);
-while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 100 por favor:"));
-    console.log(numeroUsuario);
-/*
-Este código realiza
-la comparación
-*/
 
-if (numeroUsuario == numeroSecreto) {
-        //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1? "turno" : "turnos"}`); 
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
-        if (numeroUsuario > numeroSecreto) {
-            alert("El número secreto es MENOR");
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
         } else {
-            alert("El número secreto es MAYOR");
+            asignarTextoElemento('p','El número secreto es mayor');
         }
-        //La condición no se cumplió
-        intentos = intentos + 1;
-        //palabraTurno = "turnos";
-        if (intentos > 15) {
-            alert("Llegaste al número de intentos máximo.");
-            break;
-        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
 
-        //alert('Lo siento, no acertaste el número. Intentalo otra vez');
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else {
+        //Si el numero generado está incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
     }
 }
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales();
